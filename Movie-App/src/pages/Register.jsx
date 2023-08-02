4aabf5512409a3b23e03f0f7146483ba6fa99c6b
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-08-02 11:39:03 +0300

    register

--- Movie-App/src/pages/Register.jsx
+++ Movie-App/src/pages/Register.jsx
@@ -11,24 +11,53 @@ const Register = () => {
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
-          <div>
-            <input name="floating_text" type="text" required />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              className="peer"
+              name="floating_text"
+              type="text"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_text">First Name</label>
           </div>
-          <div>
-            <input name="floating_text" type="text" required />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              className="peer"
+              name="floating_text"
+              type="text"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_text">Last Name</label>
           </div>
-          <div>
-            <input name="floating_email" type="email" />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              className="peer"
+              name="floating_email"
+              type="email"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_email">Email</label>
           </div>
-          <div>
-            <input name="floating_password" type="password" />
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              className="peer"
+              name="floating_password"
+              type="password"
+              placeholder=" "
+              required
+            />
             <label htmlFor="floating_password">Password</label>
           </div>
-          <button type="submit">Register</button>
-          <button type="button">
+          <button type="submit" className="btn-danger">
+            Register
+          </button>
+          <button
+            type="button"
+            className="btn-danger flex justify-between items-center"
+          >
             Continue with Google
             <GoogleIcon color="currentColor" />
           </button>
--- Movie-App/tailwind.config.js
+++ Movie-App/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
   },
   plugins: [],
-}
-
+};
